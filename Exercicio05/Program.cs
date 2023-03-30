77e097fd906ff4d1af6b0538ed2bdd4bdd84a3dd
Jesiane Straubel <jesi1951@hotmail.com>
2023-03-30 15:48:10 -0300

    exercicios 5 e 6

---
+++ Exercicio05/Program.cs
@@ -0,0 +1,68 @@
+﻿/*5. Crie um programa em C# que solicite ao usuário que digite uma frase. Em seguida, o programa deve utilizar
+métodos da classe string para realizar as seguintes operações:
+ Imprimir a frase em letras maiúsculas;
+ Imprimir a frase em letras minúsculas;
+ Imprimir a quantidade de caracteres da frase;
+ Imprimir a primeira palavra da frase;
+ Imprimir a última palavra da frase.*/
+using System.Globalization;
+
+namespace Exercicio05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase");
+            string frase = Console.ReadLine();
+
+            Console.ReadLine();
+
+            //Imprimir a frase em letras maiúsculas;
+
+            Console.WriteLine("- Imprime a frase em letras maiúsculas -");
+            Console.WriteLine(frase.ToUpper());
+
+            Console.ReadLine();
+
+            //Imprimir a frase em letras minúsculas;
+
+            Console.WriteLine("- Imprime a frase em letras minisculas -");
+            Console.WriteLine(frase.ToLower());
+
+            Console.ReadLine();
+
+            //Imprimir a quantidade de caracteres da frase;
+
+            int palavras = 1;
+            int caracteresTotal = 1;
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if (frase[i] == ' ' || frase[i] == '.')
+                {
+                    palavras++;
+                }
+            }
+            int[] PalavrasCaracteres = new int[2];
+            caracteresTotal = frase.Length - palavras;
+            PalavrasCaracteres[1] = caracteresTotal;
+
+            Console.WriteLine("- quantidade de caracteres da frase -");
+            Console.WriteLine(caracteresTotal);
+
+            Console.ReadLine();
+            //Imprimir a primeira palavra da frase;
+
+            Console.WriteLine("- Imprime apenas a primeira palavra da frase -");
+            Console.Write(frase.Remove(frase.IndexOf(" ")));
+
+            Console.ReadLine();
+            //Imprimir a última palavra da frase
+
+            Console.WriteLine("- Imprime apenas a ultima palavra da frase -");
+            Console.Write(frase.Remove(frase.LastIndexOf(" ")));
+
+            Console.ReadLine();
+        }
+    }
+}
--- ManipulacaoStrings/Program.cs
+++ ManipulacaoStrings/Program.cs
@@ -1,30 +1,96 @@
-﻿using System.Text;
-
+﻿/* Desenvolva um programa em C# que leia o arquivo cidades.csv 
+ e dê a possibilidade de apresentar as cidades:
+    1) agrupadas pela primeira letra 
+    2) ou pelo estado que pertence.
+ */
 namespace ManipulacaoStrings
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-         
-                string linha = "";
-                string[] linhaseparada = null;
-                StreamReader reader = new StreamReader(@"C:\Users\jesi1\OneDrive\Documentos\Projetos\AcademiaProgramador\Cidades.csv", Encoding.UTF8, true);
-                while (true)
-                {
-                    linha = reader.ReadLine();
-                    if (linha == null) break;
-                    linhaseparada = linha.Split(';');
-                    string resultado = string.Format(
-                    @"Linha - 
-                    Campo 1: {0}
-                    Campo 2: {1}
-                    Campo 3: {2}
-                    Campo 4: {3}
-                    Campo 5: {4}", linhaseparada[0], linhaseparada[1], linhaseparada[2], linhaseparada[3], linhaseparada[4]);
-                    Console.WriteLine(resultado);
-                }
-                Console.ReadKey();
+
+            //Ler o arquivo
+            string caminhoarquivo = @"C:\Users\jesi1\OneDrive\Documentos\Projetos\AcademiaProgramador\Cidades.csv";
+            string arquivo = File.ReadAllText(caminhoarquivo);
+
+            //precisamos pegar as linhas do arquivo
+
+            string[] linhas = arquivo.Split("\r\n");
+
+            //pegar a cidade e o estado : "Lages; Santa Catarina"
+
+            string[] cidadesEhEstados = new string[linhas.Length];
+
+            int posicaoCidade = 2;
+            int posicaoEstado = 3;
+
+            int j = 0;
+            for (int i = 1; i < linhas.Length; i++)
+            {
+                string[] colunas = linhas[i].Split(";");
+
+                cidadesEhEstados[j] = colunas[posicaoCidade] + ";" + colunas[posicaoEstado];
+
+                j++;
             }
+
+            //mostrar cidades agrupadas pela letra
+            char[] alfabeto = new char[]
+            {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                   'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+            };
+
+            for (int i = 0; i < alfabeto.Length; i++)
+            {
+                //mostrar o cabeçalho
+                Console.WriteLine($"\nCidades com a letra {alfabeto[i]}\n");
+
+                for (int x = 0; x < cidadesEhEstados.Length; x++)
+                {
+                    char primeiraLetra = alfabeto[i];
+
+                    //verificar se o nome do municipio inicia com a letra selecionada
+                    if (cidadesEhEstados[x] != null && cidadesEhEstados[x].StartsWith(primeiraLetra))
+                    {
+                        int posicaoInicioEstado = cidadesEhEstados[x].IndexOf(";");
+                        string cidadeSemEstado = cidadesEhEstados[x].Remove(posicaoInicioEstado);
+                        Console.WriteLine("\t" + cidadeSemEstado);
+                    }
+                }
+            }
+
+            Console.Clear();
+
+            string[] estados = new string[]
+            {
+                "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal",
+                "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul",
+                "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro",
+                "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina",
+                "São Paulo", "Sergipe", "Tocantins"
+            };
+
+            for (int i = 0; i < estados.Length; i++)
+            {
+                //mostrar o cabeçalho
+                string estado = estados[i];
+
+                Console.WriteLine($"\nCidades do estado: {estado}\n");
+
+                for (int x = 0; x < cidadesEhEstados.Length; x++)
+                {
+                    if (cidadesEhEstados[x] != null && cidadesEhEstados[x].Contains(estado))
+                    {
+                        int posicaoInicioEstado = cidadesEhEstados[x].IndexOf(";");
+                        string cidadeSemEstado = cidadesEhEstados[x].Remove(posicaoInicioEstado);
+                        Console.WriteLine("\t" + cidadeSemEstado);
+                    }
+                }
+            }
+
+            Console.ReadLine();
         }
     }
+}
